91395ea059413d247c04ea2bf40aa5f6c1e7e84e
ybourkeb-ensae <6002584+yrahan@users.noreply.github.com>
2024-06-25 09:39:50 +0000

    mise en forme

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import panel as pn
+
 pn.extension()
+
+
 @pn.cache
 def get_data():
     return pd.read_csv("faithful.csv")
+
 
 def get_faithful_hist(bins):
     faithful = get_data()
     fig, ax = plt.subplots()
     ax.hist(faithful["waiting"], bins=bins, color="orange")
-    plt.close(fig) # Évite les fuites de mémoire
+    plt.close(fig)  # Évite les fuites de mémoire
     return fig
+
+
 bins = pn.widgets.IntSlider(name="Nombre de blocs", start=1, end=50, value=30)
 faithful_hist = pn.bind(get_faithful_hist, bins=bins)
 pn.template.FastListTemplate(
-title="Geyser Old Faithful ￿",
-sidebar=[bins],
-main=[pn.pane.Matplotlib(faithful_hist)],
+    title="Geyser Old Faithful ￿",
+    sidebar=[bins],
+    main=[pn.pane.Matplotlib(faithful_hist)],
 ).servable()
